ea3f4ef2ac225293ca3868c1f8ab81f7dce5c0e5
Angel-ST221 <whtlswk0@gmail.com>
2015-05-23 19:16:05 +0900

    Update 0xc1900106.cmd

--- 0xc1900106.cmd
+++ 0xc1900106.cmd
@@ -1,1 +1,21 @@
-dsfgsdfgd
+@echo off
+net stop wuauserv
+echo.
+net stop cryptSvc
+echo.
+net stop bits
+echo.
+net stop msiserver
+echo.
+ren C:\Windows\SoftwareDistribution SoftwareDistribution.old
+ren C:\Windows\System32\catroot2 catroot2.old
+net start wuauserv
+echo.
+net start cryptSvc
+echo.
+net start bits
+echo.
+net start msiserver
+echo.
+pause
+exit
